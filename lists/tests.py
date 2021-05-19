64127946c3aa1d8cb44e97aa0601cb97893677ee
overfitted <overfitted@gmail.com>
2021-05-20 04:26:17 +0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page  
 
@@ -10,10 +11,29 @@ class HomePageTest(TestCase):
         found = resolve('/')  
         self.assertEqual(found.func, home_page)
         
+    # direct testing of content of response page, TESTING CONSTANTS
+    '''    
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()  
         response = home_page(request)  
         html = response.content.decode('utf8')  
         self.assertTrue(html.startswith('<html>'))  
         self.assertIn('<title>To-Do lists</title>', html)  
-        self.assertTrue(html.endswith('</html>')) 
+        self.assertTrue(html.strip().endswith('</html>')) 
+    '''
+     
+    # compare to equivalent file
+    # weakness: need to decode ourself, craft our own httprequest
+    '''
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+    '''
+     
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')  
+        # from djangotestsuite
+        self.assertTemplateUsed(response, 'home.html')  
