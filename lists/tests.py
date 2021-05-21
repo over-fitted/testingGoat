34deaf5cda073079925bb8bee4ce2ce18242c55b
overfitted <overfitted@gmail.com>
2021-05-21 21:27:45 +0800

    works for first input using magic number as list index

--- lists/tests.py
+++ lists/tests.py
@@ -32,8 +32,13 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(html, expected_html)
     '''
-     
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')  
+        
+    def test_uses_home_template(self):
+        response = self.client.get('/')
         # from djangotestsuite
-        self.assertTemplateUsed(response, 'home.html')  
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    # second arg = which template to return
+    # third arg = dict binding variables to vals
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+        # .get instead of ['item_text] to supply default value as second arg
+    })
