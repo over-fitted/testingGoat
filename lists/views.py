332808aa69e0587e1a441725db34a9e40d59b9a8
overfitted <overfitted@gmail.com>
2021-05-24 04:54:29 +0800

    new list redirect, seperated home and list template

--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,11 @@ def home_page(request):
         # creates new object and saves it
         Item.objects.create(text=request.POST['item_text'])
         # always redirect after post request
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
+
+
