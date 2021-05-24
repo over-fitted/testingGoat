293db18c691b1278161f1ed8b38a274c53b49c2d
overfitted <overfitted@gmail.com>
2021-05-25 04:20:32 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,9 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
     url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    # (__) capture group, pass the string as a 2nd argument for view_list
+    # use \d to only capture numberical id
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
