071924cf27fabb687946b06fa252cb3f193943ab
overfitted <overfitted@gmail.com>
2021-05-24 19:17:47 +0800

    seperated creating of new list to /lists/new

--- lists/tests.py
+++ lists/tests.py
@@ -38,22 +38,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         # from djangotestsuite
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)  
-        new_item = Item.objects.first()  
-        self.assertEqual(new_item.text, 'A new list item')  
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        # checks for redirect to '/', redirect code is 302
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-        
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
         
 class ItemModelTest(TestCase):
 
@@ -89,4 +73,15 @@ class ListViewTest(TestCase):
         self.assertIn('Itemey 1', response.content.decode())
         self.assertIn('Itemey 2', response.content.decode())
     
-    
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)  
+        new_item = Item.objects.first()  
+        self.assertEqual(new_item.text, 'A new list item')  
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        # checks for redirect to '/', redirect code is 302
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from lists.models import Item
 
-def home_page(request):    
-    if request.method == 'POST':
-        # creates new object and saves it
-        Item.objects.create(text=request.POST['item_text'])
-        # always redirect after post request
-        return redirect('/lists/the-only-list-in-the-world/')
+def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
+
 
 
