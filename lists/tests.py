7e03f95d78274ab1df930036bcd788a3a3af5dd0
overfitted <overfitted@gmail.com>
2021-05-24 19:56:46 +0800

    adjusted model to associate items with parent lists

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest, response
 from django.template.loader import render_to_string
 
 from lists.views import home_page  
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -39,25 +39,36 @@ class HomePageTest(TestCase):
         # from djangotestsuite
         self.assertTemplateUsed(response, 'home.html')
         
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
 
-        # returns records as a list-like queryset
+        # check that list is saved correctly
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+        
+        # returns records as a list-like queryset, check that items saved correctly
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
         
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -65,8 +76,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
         
     def test_displays_all_items(self):
-        Item.objects.create(text='Itemey 1')
-        Item.objects.create(text='Itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='Itemey 1', list=list_)
+        Item.objects.create(text='Itemey 2', list=list_)
         
         response = self.client.get('/lists/the-only-list-in-the-world/')
         
